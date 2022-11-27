1cf8034358c23235bd3e13fe2c0b1f89e2f0de72
Yiannis Charalambous <yiannis128@hotmail.com>
2022-11-27 02:30:34 +0000

    Added to map method to make it compatible with DigitalOceans

--- response.go
+++ response.go
@@ -1,5 +1,7 @@
 package godo
 
+import "encoding/json"
+
 // Response returns back the http code, type of data, and the presigned url to the user.
 type Response struct {
 	// StatusCode is the http code that will be returned back to the user.
@@ -18,4 +20,15 @@ func NewResponseJSON() Response {
 
 func (r *Response) AddHeader(key, value string) {
 	r.Headers[key] = value
+}
+
+func (r *Response) ToMap() map[string]interface{} {
+	if rJSON, err := json.Marshal(r); err != nil {
+		var output map[string]interface{}
+		if err = json.Unmarshal(rJSON, &output); err != nil {
+			return output
+		}
+	}
+
+	return nil
 }
